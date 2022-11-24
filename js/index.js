563f13a7c2226e5e74a4731f2e1a81ae07226b21
Nico <2629486708@qq.com>
2022-11-24 08:53:47 +0800

    修改hover效果

--- js/index.js
+++ js/index.js
@@ -1,6 +1,19 @@
 ;(() => {
+  const header = document.querySelector('header')
+  const goBack = document.getElementById('go-back')
+  const back = document.querySelector('.btn-go-top')
+  const topContent = document.getElementById('top-warp')
+
   window.addEventListener('scroll', () => {
-    let header = document.querySelector('header')
     header.classList.toggle('header-sticky', window.scrollY > 0)
+    back.classList.toggle('goback-show', window.scrollY > 800)
   })
+
+  // 返回顶部
+  goBack.onclick = () => {
+    console.log(123)
+    topContent.scrollIntoView({
+      behavior: 'smooth'
+    })
+  }
 })()
